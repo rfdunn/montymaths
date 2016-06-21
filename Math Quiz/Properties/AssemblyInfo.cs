20e8e6ea68d0da01ddca423d60aa2ad3769daa26
Maccgyver <russell.dunn@red-gate.com>
2016-06-21 13:52:42 +0100

    Initial commit

---
+++ Math Quiz/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Assembly info
+[assembly: AssemblyTitle("Montymaths")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Montymaths")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// COM Visibility
+[assembly: ComVisible(false)]
+
+// GUID for the ID of the typelib (if this project is exposed to COM)
+[assembly: Guid("299d557f-f58e-4157-a755-7d703d47bc70")]
+
+// Version info:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
